4034da0a16f0e859dc08cf97f70ad0681cde16ef
Rahim10020 <rahialighi@gmail.com>
2025-01-27 15:30:36 +0000

    Premier commit

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+
+function Food(){
+    const food1 = "Pizza"
+    const food2 = "Burger"
+    return (
+        <ul>
+            <li>Apple</li>
+            <li>{food1.toUpperCase()}</li>
+            <li>{food2}</li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
+
+export default Food
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header() {
+    return (
+      <header>
+        <h1>My Web site</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">A Propos</a></li>
+            </ul>
+        </nav>
+      </header>
+    );
+  }
+  export default Header
+    
